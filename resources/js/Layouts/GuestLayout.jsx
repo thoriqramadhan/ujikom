ad210ac1d159892d09e21f2708870975c05af1b6
Thoriq R <respect.thoriq@gmail.com>
2024-03-21 14:41:48 +0500

    adding(Progress) resonsive for login

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,7 +2,10 @@
 
 export default function Guest({ children }) {
     return (
-        <div className="w-full h-[100vh] md:bg-sky-500">
+        <div className="w-full h-[100vh] sm:bg-sky-500 sm:flex">
+            <div className="w-[200px] h-[200px] bg-red-100 hidden sm:block">
+
+            </div>
             <div className="h-[100vh] sm:max-w-md flex flex-col justify-center px-[30px] bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
