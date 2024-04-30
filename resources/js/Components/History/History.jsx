7c4cb5fbd28dfbc3abb8feee6a023539cffee0ab
Thoriq R <respect.thoriq@gmail.com>
2024-04-30 11:53:43 +0300

    init for data and modal in history

--- resources/js/Components/History/History.jsx
+++ resources/js/Components/History/History.jsx
@@ -1,63 +1,81 @@
 import BodyLayout from '@/Layouts/BodyLayout'
-import React from 'react'
+import React, { useEffect } from 'react'
 import LogoDate from '../Logo_date'
 import SearchSvg from '../svgComp/SearchSvg'
 import TextInput from '../TextInput'
 import TableData from '../TableData'
 import { useState } from 'react';
+import DashedLine from '../DashedLine'
 
-const posts = [
-  {customer_name : 'customer_name',
-  order_time :'order_time',
-  status: 'status'},
-  // {customer_name : 'thoriq',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'thoriq',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'thoriq',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'thoriq',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'thoriq',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // {customer_name : 'yones',
-  // order_time :'08.00',
-  // status: 'lunas'},
-  // ...data lainnya
+const itemPost = [
+  {
+    id: 1,
+    name: 'Thoriq',
+    subTotal : `100K`,
+    tax: `10K`,
+    total: `110K`,
+    menu: [
+      {
+        name : 'Ayam',
+        total: 100,
+        items: 1
+      },
+      {
+        name : 'Sapi',
+        total: 200,
+        items: 2
+      },
+      {
+        name : 'Ayam',
+        total: 300,
+        items: 3
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Thor',
+    subTotal : `100K`,
+    tax: `10K`,
+    total: `110K`,
+    menu: [
+      {
+        name : 'Ayam',
+        total: 100,
+        items: 2
+      },
+      {
+        name : 'Babi',
+        total: 100,
+        items: 2
+      },
+      {
+        name : 'Ayam',
+        total: 100,
+        items: 2
+      }
+    ]
+  },
 ];
 
-function History({orderselesai}) {
+function History({orderselesai , orderitems}) {
   const [dataOrderSelesai , setDataOrderSelesai] = useState(orderselesai || '')
   console.log(dataOrderSelesai)
   const [currentPage , setcurrentPage] = useState(1)
   const [postPerPage,  setPostPerPage] = useState(5)
+  const [menu , setMenu] = useState({})
+  const [bill , setBill] = useState({
+    total:0,
+    tax:0,
+  })
+  const [customerName,setCustomerName] = useState('jo')
+  const [openModal , setOpenModal] = useState(true)
   const pageNumbers = []
 
-  for (let i = 1; i <= Math.ceil(posts.length / postPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(dataOrderSelesai.length / postPerPage); i++) {
     pageNumbers.push(i);
   }
-  // console.log(posts.length / postPerPage)
-  // console.log(pageNumbers)
+  console.log(orderselesai , orderitems)
   
 
   function incrementHandler(){
@@ -74,8 +92,26 @@ function History({orderselesai}) {
   }
   const indexOfLastPost = currentPage * postPerPage;
   const indexOfFirstPost = indexOfLastPost - postPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = dataOrderSelesai.slice(indexOfFirstPost, indexOfLastPost);
+
+  function detailHandler(id){
+    console.log(id)
+    const menu = orderitems.find(items => items.order_id === id)
+    const name = orderselesai.find(items => items.id === id)
+    setOpenModal(!openModal)
+    setMenu(menu)
+    setCustomerName(name.customer_name)
+
+  }
   
+  useEffect(()=>{
+    const bill = orderitems.reduce((accumulator,order) => accumulator + order.totalHarga, 0)
+    const tax = bill * 0.1
+    setBill({
+      total: bill,
+      tax:tax
+    })
+  },[menu])
 
   return (
     <BodyLayout className={'pt-[40px] px-[40px]'}>
@@ -98,25 +134,80 @@ function History({orderselesai}) {
           <th className='flex-2 opacity-60'>Status</th>
           <th></th>
         </tr>
-        {dataOrderSelesai.map(orderselesai => {
+        {
+        dataOrderSelesai.length == 0 ? <p>Tidak ada data</p>  :
+         dataOrderSelesai.map(orderselesai => {
         return (
           <tr className='h-fit border-bottom-1'>
           <TableData text={orderselesai.customer_name}/>
-          <TableData text={orderselesai.order_time}/>
-          <TableData text={orderselesai.status}/>
+          <TableData text={orderitems.subTotal}/>
+          <TableData text={orderitems.totalHarga} prop='K'/>
           <td>
-          <div className="h-[60px] w-[100%] flex items-center justify-center">
+          <div className="h-[60px] w-[100%] flex items-center justify-center" onClick={()=>{detailHandler(orderselesai.id)}}>
             <button className='w-[100px] py-[7px] bg-[#E8E8E8] rounded-lg border-gray-400 border'> 
             <span className='mr-[2px]'>I</span>
-            <span className="opacity-60">Edit</span>
+            <span className="opacity-60">Detail</span>
             </button>
           </div>
           </td>
         </tr>
         )
-      })}
+      })
+      }
         
       </table>
+
+          {/* modal */}
+          <div className={`w-[900px] h-[500px] px-[25px] py-[25px] flex flex-col items-center absolute left-1/2 right-1/2 -translate-x-1/2 transition-all duration-1000 rounded-xl bg-white border shadow-lg ${openModal ? '-translate-y-[1000px]' : '-translate-y-0' }`}>
+              <p className='text-xl font-bold'>Detail Pesanan</p>
+              <p>Detail Pesanan punya {customerName}😁</p>
+              <div className="w-full h-full mt-[10px] flex gap-x-[30px]">
+              <div className="basis-2 flex-1 bg-white rounded-xl border ">
+                <table className='w-full h-fit rounded-xl overflow-hidden bg-white'>
+                  <tr className='rounded-2xl bg-[#F3F3F3] h-[60px]'>
+                    <th className='flex-1 opacity-60'>Nama Makanan</th>
+                    <th className='flex-1 opacity-60'>Jumlah</th>
+                    <th className='flex-1 opacity-60'>Harga</th>
+                  </tr>
+                  {
+                    orderselesai.map((orderitems , index) => {
+                      return (
+                      <tr key={index} className='h-fit border-bottom-1'>
+                        <TableData text={menu.name}/>
+                        <TableData text={menu.items}/>
+                        <TableData text={menu.totalHarga}/>
+                      </tr>
+                      )
+                    }
+                    )
+                  
+                  }
+                  </table>
+              </div>
+
+              <div className="w-[330px] h-full border rounded-xl bg-white flex flex-col  py-[20px] px-[25px]">
+                {itemPost.length == 0 ? <p>TIdak ada data</p> : (
+                  <div className='flex-1'>
+                  <div className="w-full flex justify-between">
+                  <p className='opacity-30 font-bold'>Sub Total</p>
+                  <p className='font-bold'>{bill.total}K</p>
+                </div>
+                <div className="w-full flex justify-between">
+                  <p className='opacity-30 font-bold'>{'Pajak (10%)'}</p>
+                  <p className='font-bold'>{bill.tax.toFixed(2)}K</p>
+                </div>
+                <DashedLine />
+                <div className="w-full flex justify-between mt-[20px]">
+                  <p className='opacity-30 font-bold'>Total</p>
+                  <p className='font-bold'>{bill.total + bill.tax}K</p>
+                </div>
+                  </div>
+                )
+                }
+                <button className='w-full rounded-[18px] py-[15px] font-bold text-[#797979] bg-[#F3F3F3] border-2' onClick={() => setOpenModal(!openModal)}>Kembali ke History</button>
+              </div>
+            </div>
+          </div>
     </BodyLayout>
     )
 }
