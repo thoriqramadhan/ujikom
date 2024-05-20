e02d0a07427b192add5468f0bab4747b42c0fa18
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-05-20 16:42:38 +0300

    Seeder User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Faker\Generator;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'first_name'    =>  'Test',
+            'last_name'     =>  'User',
+            'email'         =>  'test@gmail.com',
+            'password'      =>  Hash::make('123456789'),
+            'role'          =>  'admin',
+            'last_seen'     =>  null
+        ]);
     }
 }
