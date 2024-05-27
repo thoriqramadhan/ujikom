909e355e46f54c850a2581f9757c909963c3acec
Thoriq R <respect.thoriq@gmail.com>
2024-05-27 08:58:48 +0300

    Fixing chart

--- resources/js/Components/Home(Admin)/TabData.jsx
+++ resources/js/Components/Home(Admin)/TabData.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TabData({day , value}) {
   return (
-<div className={`w-[45px] h-[25%] bg-[#C5A68B] relative group ml-[8%]`}>
+<div className={`w-[45px] bg-[#C5A68B] relative group ml-[8%]`} style={{height : `${value}%`}}>
         <p className='absolute -bottom-8 text-sm opacity-0 group-hover:opacity-100 font-bold'>{day}</p>
         <p className='absolute -bottom-12 text-sm opacity-0 group-hover:opacity-100 text-center'>{value}%</p>
     </div>
