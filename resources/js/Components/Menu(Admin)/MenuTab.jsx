87bd11b44b25e9ba929ff8cbc80a115045ea344d
Thoriq R <respect.thoriq@gmail.com>
2024-05-28 09:50:45 +0300

    change categories input to select , set categories by database

--- resources/js/Components/Menu(Admin)/MenuTab.jsx
+++ resources/js/Components/Menu(Admin)/MenuTab.jsx
@@ -7,13 +7,19 @@ import Checklist from "../svgComp/Checklist";
 function MenuTab({ searchOutput, categories, menus }) {
     const [activeTab, setActiveTab] = useState("Semua");
     const [menu, setMenu] = useState(menus);
-    const [openModalEditMenu, setOpenModalEditMenu] = useState(true);
+    const [openModalEditMenu, setOpenModalEditMenu] = useState(false);
+
+    
     // dataModal
     const [dataModalId, setDataModalId] = useState();
     const [dataModalMenu, setdataModalMenu] = useState();
     const [dataModalKategori, setdataModalKategori] = useState();
     const [dataModalHarga, setdataModalHarga] = useState();
-    console.log(menu);
+    // modal edit menu
+    const [categoriesArr , setCategoriesArr] = useState(categories.map(item => item.kategori))
+    const [categorieNow , setCategorieNow] = useState('')
+
+    console.log(categories);
     useEffect(() => {
         if (activeTab != "Semua") {
             const categorie = categories.find(
@@ -32,11 +38,16 @@ function MenuTab({ searchOutput, categories, menus }) {
         setter(value)
     }
 
-    function detailHandler(id, nama, categories_id,harga) {
+    function detailHandler(id, nama, categories_id, harga) {
         setOpenModalEditMenu(!openModalEditMenu);
         setDataModalId(id);
         setdataModalMenu(nama);
         setdataModalKategori(categories_id);
+
+        const dataObj = menu.find(item => item.id == id)
+        const categorieObj = categories.find(categorie => categorie.id == dataObj.categories_id)
+        setCategorieNow(categorieObj.kategori)
+
         setdataModalHarga(harga);
         console.log("setDataModal:", harga)
     }
@@ -128,17 +139,29 @@ function MenuTab({ searchOutput, categories, menus }) {
                     ))
                 )}
             </div>
+
+            {/* modal */}
             <div
-                className={`absolute top-0 left-0 w-full h-full flex justify-center duration-1000  ${
+                className={`absolute top-0 left-0 w-full h-full flex justify-center mt-4 duration-1000 ${
                     openModalEditMenu
                         ? "-translate-y-[1000px]"
                         : "translate-y-10 fixed"
                 } transition-all`}
             >
-                <div className="flex-row bg-white w-[300px] h-[440px] mt-[50%] rounded-[20px] border-2 border-[#d9d9d9] md:mt-[5%] md:w-[500px] md:h-[500px] justify-center">
-                    <div className="flex-col mx-auto p-5 text-center">
+                <div className="z-10 flex-row bg-white w-[80vw] h-[440px] rounded-[20px] border-2 border-[#d9d9d9] md:mt-[5%] md:w-[500px] md:h-[500px] justify-center">
+                    <div className="flex-col mx-auto p-5 text-center relative">
                         <p className="font-bold text-2xl">Edit Menu</p>
                         <p>Edit Menu anda disini! 👳🏻</p>
+                        <div className="absolute top-3 right-5 w-fit h-fit">
+                            <button
+                                className="text-2xl text-red-600 font-black md:text-3xl"
+                                onClick={() => {
+                                    setOpenModalEditMenu(!openModalEditMenu);
+                                }}
+                            >
+                                x
+                            </button>
+                </div>
                     </div>
                     <div className="bg-[#d9d9d9] w-[120px] h-[120px] mx-auto rounded-xl"></div>
                     <div className="flex-col text-center mt-3 md:px-[10px] md:mt-10 md:mx-[75px] mx-[40px]">
@@ -150,14 +173,11 @@ function MenuTab({ searchOutput, categories, menus }) {
                             value={dataModalMenu}
                             required
                         />
-                        <input
-                            type="text"
-                            className="mt-1 rounded-xl  md:mt-3 w-full"
-                            placeholder="Kategori"
-                            onChange={(e) => {inputHandler(e.target.value, setdataModalKategori)}}
-                            value={dataModalKategori}
-                            required
-                        />
+                        <select name="" id="" value={categorieNow} onChange={(e) => setCategorieNow(e.target.value)} className='mt-1 rounded-xl  md:mt-3 w-full'>
+                            {categoriesArr.map(categorie => (
+                                <option value={categorie}>{categorie}</option>
+                            ))}
+                        </select>
                         <div className="flex w-full h-full">
                         <p className="text-xl my-auto pl-2 pr-2">Rp.</p>
                         <input
@@ -180,16 +200,7 @@ function MenuTab({ searchOutput, categories, menus }) {
                         </button>
                     </div>
                 </div>
-                <div className="absolute top-[190px] right-[53px] w-fit h-fit md:top-[87px] md:right-[560px]">
-                    <button
-                        className="text-2xl text-red-600 font-black md:text-3xl"
-                        onClick={() => {
-                            setOpenModalEditMenu(!openModalEditMenu);
-                        }}
-                    >
-                        x
-                    </button>
-                </div>
+                
             </div>
         </>
     );
