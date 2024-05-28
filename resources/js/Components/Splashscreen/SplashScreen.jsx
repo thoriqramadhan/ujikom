08a76fdc71b964b0107d5d86242e2fccce585bc1
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-05-28 12:25:15 +0300

    SplashScreen

---
+++ resources/js/Components/Splashscreen/SplashScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SplashScreen(){
+
+
+    return(
+        <div class="flex-col text-center mt-[10%] h-[100vh]">
+    <img src="./123.png" alt="SVG" class="h-[250px] w-[250px] mx-auto "/>
+    <p class="font-bold text-2xl mt-10">Selamat Datang, Fachry!</p>
+    <p class="text-[#5f83a3] text-md">Tunggu sebentar sampai tampil muncul...</p>
+    </div>
+    )
+}
+
+    
