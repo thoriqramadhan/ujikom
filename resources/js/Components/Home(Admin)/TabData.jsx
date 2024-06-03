598f60d070b9ba8c580de594642a7bc5528da0e0
Thoriq R <respect.thoriq@gmail.com>
2024-06-03 15:25:06 +0300

    chart update

--- resources/js/Components/Home(Admin)/TabData.jsx
+++ resources/js/Components/Home(Admin)/TabData.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { formatRupiah } from '@/module/rupiah-formater'
 
-function TabData({day , value}) {
-  let values = value
+function TabData({day , percentage, value}) {
+  let values = percentage
   if(values >= 100){
     values = 100
   }
@@ -9,7 +10,8 @@ function TabData({day , value}) {
     <>
     <div className={`w-[45px] bg-[#C5A68B] relative group ml-[8%]`} style={{ height: `${values}%` }}>
         <p className='absolute -bottom-8 text-sm opacity-0 group-hover:opacity-100 font-bold'>{day}</p>
-        <p className='absolute -bottom-12 text-sm opacity-0 group-hover:opacity-100 text-center'>{value}%</p>
+        <p className='absolute -bottom-12 text-sm opacity-0 group-hover:opacity-100 text-center'>{percentage}%</p>
+        <p className='absolute -bottom-[70px] text-sm opacity-0 group-hover:opacity-100 text-center'>{formatRupiah(value)}</p>
     </div>
     </>
   )
