905143f7f4b40f18f34c6a747b5af81085345388
Thoriq R <respect.thoriq@gmail.com>
2024-06-03 11:45:49 +0300

    chart indicator fix

--- resources/js/Components/Home(Admin)/Chart.jsx
+++ resources/js/Components/Home(Admin)/Chart.jsx
@@ -3,24 +3,18 @@ import React, { useEffect, useState } from 'react'
 import ChartIndicator from './ChartIndicator'
 import TabData from './TabData'
 
-function Chart({dailyIncome}){
+function Chart ({dailyIncome}){
+  const [dailyIncomes , setDailyIncomes] = useState(dailyIncome || [])
   const dailyTarget = 1000000;
   const dailyPercentage = (value , target) => {
     return Math.round((value / target) * 100);
   }
-  const [dailyIncomes , setDailyIncomes] = useState({
-    monday: dailyIncome?.Monday || 0,
-    tuesday: dailyIncome?.Tuesday || 0,
-    wednesday: dailyIncome?.Wednesday || 0,
-    thursday : dailyIncome?.Thursday || 0,
-    friday : dailyIncome?.Friday || 0,
-    saturday : dailyIncome?.Saturday || 0,
-    sunday : dailyIncome?.Sunday || 0
-  })
-  useEffect(()=>{
-    setDailyIncomes(dailyIncomes)
-  })
-  console.log(dailyIncomes , dailyIncome)
+  useEffect(() => {
+    if (dailyIncome) {
+      setDailyIncomes(dailyIncome);
+    }
+  }, [dailyIncome]);
+  
   return (
     <div className="w-full h-fit mt-[15px] pt-[35px] pb-[50px] bg-white border px-[30px] rounded-xl relative">
           <div className="mb-[10px]">
@@ -29,13 +23,9 @@ function Chart({dailyIncome}){
           </div>
           <div className="w-full h-[286px] pt-[20px] flex justify-evenly items-end  relative">
             <ChartIndicator/>
-            <TabData day={'Senin'} value={dailyPercentage(dailyIncomes.monday , dailyTarget)}/>
-            <TabData day={'Selasa'} value={dailyPercentage(dailyIncomes.tuesday , dailyTarget)}/>
-            <TabData day={'Rabu'} value={dailyPercentage(dailyIncomes.wednesday , dailyTarget)}/>
-            <TabData day={'Kamis'} value={dailyPercentage(dailyIncomes.thursday , dailyTarget)}/>
-            <TabData day={"Jum'at"} value={dailyPercentage(dailyIncomes.friday , dailyTarget)}/>
-            <TabData day={'Sabtu'} value={dailyPercentage(dailyIncomes.saturday , dailyTarget)}/>
-            <TabData day={'Minggu'} value={dailyPercentage(dailyIncomes.sunday , dailyTarget)}/>
+            {dailyIncomes.map((item, index) => (
+              <TabData key={index} day={item.day} value={dailyPercentage(item.value , dailyTarget)} />
+            ))}
           </div>
 
         </div>
--- resources/js/Components/Home(Admin)/TabData.jsx
+++ resources/js/Components/Home(Admin)/TabData.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 
 function TabData({day , value}) {
+  let values = value
+  if(values >= 100){
+    values = 100
+  }
   return (
     <>
-    {value === 0 ? 
-      <div className={`w-[45px] relative group ml-[8%]`} style={{ height: `100%` }}>
-        <p className='absolute -bottom-8 text-sm opacity-0 group-hover:opacity-100 font-bold'>{day}</p>
-        <p className='absolute -bottom-12 text-sm opacity-0 group-hover:opacity-100 text-center'>{value}%</p>
-      </div>
-      :
-      <div className={`w-[45px] bg-[#C5A68B] relative group ml-[8%]`} style={{ height: `${value}%` }}>
+    <div className={`w-[45px] bg-[#C5A68B] relative group ml-[8%]`} style={{ height: `${values}%` }}>
         <p className='absolute -bottom-8 text-sm opacity-0 group-hover:opacity-100 font-bold'>{day}</p>
         <p className='absolute -bottom-12 text-sm opacity-0 group-hover:opacity-100 text-center'>{value}%</p>
     </div>
-    } 
-    
     </>
   )
 }
